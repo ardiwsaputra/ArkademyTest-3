77f761324d85f7cc97280218479035f019876aed
Ardi Widyanto Saputra <ardiwidyanto15@gmail.com>
2019-07-13 10:41:10 +0700

    Soal Kedua

---
+++ 2.php
@@ -0,0 +1,31 @@
+
+<?php
+
+    function is_usernamel_valid($username)
+    {
+        if(preg_match("/^([a-zA-Z][a-zA-Z0-9]{5,9}$)+/", $username)) 
+        {
+            echo "VALID";
+        } else {
+            echo "NOT VALID";
+        }
+    }
+
+    function is_password_valid($password)
+    {
+        if(preg_match("/^(?=.{8,}$)(?=.[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?\W).*$/", $password)) 
+        {
+            echo "VALID";
+        } else {
+            echo "NOT VALID";
+        }
+    }
+
+     is_usernamel_valid('89arDiw');
+        echo "\n";
+     is_usernamel_valid('arDiw89');
+        echo "\n";
+     is_password_valid('akulapaR1@#');
+        echo "\n";
+     is_password_valid('akulapar');
+?>
